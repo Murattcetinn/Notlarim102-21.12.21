32c475863e8f2d180c1b8ea4e52873e837ef31b9
murattcetinn <muratcetin7@outlook.com>
2021-12-21 17:33:27 +0300

    Add project files.

---
+++ Notlarim102.WebApp/Controllers/CategoryController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Notlarim102.BusinessLayer;
+using Notlarim102.Entity;
+
+namespace Notlarim102.WebApp.Controllers
+{
+    public class CategoryController : Controller
+    {
+        // GET: Category
+        
+    }
+}
---
+++ Notlarim102.WebApp/Controllers/HomeController.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Notlarim102.BusinessLayer;
+using Notlarim102.Entity;
+using Notlarim102.Entity.ValueObject;
+
+namespace Notlarim102.WebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            //Test test = new Test();
+            ////test.InsertTest();
+            ////test.UpdateTest();
+            ////test.DeleteTest();
+            //test.CommentTest();
+
+            //if (TempData["mm"]!=null)
+            //{
+            //    return View(TempData["mm"] as List<Note>);
+            //}
+
+
+            NoteManager nm = new NoteManager();
+
+            //return View(nm.GetAllNotes().OrderByDescending(x=>x.ModifiedOn).ToList());
+            return View(nm.GetAllNoteQueryable().OrderByDescending(x=>x.ModifiedOn).ToList());
+        }
+        public ActionResult ByCategory(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            CategoryManager cm = new CategoryManager();
+            Category cat = cm.GetCategoriesById(id.Value);
+
+            if (cat == null)
+            {
+                return HttpNotFound();
+            }
+
+            //TempData["mm"] = cat.Notes;
+
+            return View("Index",cat.Notes.OrderByDescending(x=>x.ModifiedOn).ToList());
+        }
+
+        public ActionResult MostLiked()
+        {
+            NoteManager nm = new NoteManager();
+            return View("Index",nm.GetAllNotes().OrderByDescending(x => x.LikeCount).ToList());
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(LoginViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                NotlarimUserManager num = new NotlarimUserManager();
+                BusinessLayerResult<NotlarimUser> res = num.LoginUser(model);
+                if (res.Errors.Count>0)
+                {
+                    res.Errors.ForEach(s => ModelState.AddModelError("", s));
+                    return View(model);
+
+                }
+                Session["login"] = res.Result;
+                return RedirectToAction("Index");
+            }
+            return View();
+           
+        }
+
+        public ActionResult Register()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Register(RegisterViewModel model)
+        {
+            //kullanici adinin uygunlugu
+            //email kontrolu
+            //activasyon islemi
+
+            //bool hasError = false;
+            if (ModelState.IsValid)
+            {
+                NotlarimUserManager num = new NotlarimUserManager();
+                BusinessLayerResult<NotlarimUser> res = num.RegisterUser(model);
+                if (res.Errors.Count>0)
+                {
+                    res.Errors.ForEach(s => ModelState.AddModelError("", s));
+                    return View(model);
+
+                }
+
+                //NotlarimUser user = null;
+                //try
+                //{
+                //    user = num.RegisterUser(model);
+                //}
+                //catch (Exception ex)
+                //{
+                //    ModelState.AddModelError("", ex.Message);   
+                //}
+                //if (model.Username=="aaa")
+                //{
+                //    ModelState.AddModelError("","Bu kullanici adi uygun degil..");
+                //    //hasError = true;
+                //}
+
+                //if (model.Email=="aaa@aaa.com")
+                //{
+                //    ModelState.AddModelError("","Email adresi daha once kullanilmis.Baska bir email deneyin.");
+                //    //hasError = true;
+                //}
+
+                //if (hasError==true)
+                //{
+                //    return View(model);
+                //}
+                //else
+                //{
+                //    return RedirectToAction("RegisterOk");
+                //}
+
+                //foreach (var item in ModelState)
+                //{
+                //    if (item.Value.Errors.Count > 0)
+                //    {
+                //        return View(model);
+                //    }
+                //}
+                return RedirectToAction("RegisterOk");
+
+            }
+            return View(model);
+        }
+
+        public ActionResult RegisterOk()
+        {
+            return View();
+        }
+
+            
+    }
+}
